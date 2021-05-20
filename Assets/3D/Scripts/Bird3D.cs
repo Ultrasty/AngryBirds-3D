14b46eb64dad1414226b0d4c30bdd47f459846aa
sty <1455670697@qq.com>
2021-05-20 23:53:07 +0800

    鸟会费了

--- Assets/3D/Scripts/Bird3D.cs
+++ Assets/3D/Scripts/Bird3D.cs
@@ -4,34 +4,57 @@ using UnityEngine;
 
 public class Bird3D : MonoBehaviour
 {
+
+
     public Transform ori;
     private bool isClick = false;
     // Start is called before the first frame update
+    private Rigidbody rg;
+    private SpringJoint sp;
+    private void Awake()
+    {
+        sp = GetComponent<SpringJoint>();
+        rg = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         
     }
-
+    void FixedUpdate()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (isClick)
         {
             RaycastHit hit;
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                transform.position = new Vector3(hit.point.x, hit.point.y, 8);
+                if (hit.rigidbody.name == "redbird") {
+                    transform.position = new Vector3(hit.point.x, hit.point.y, 8);
+                }
             }
         }
     }
-    
+
     private void OnMouseDown()
     {
         isClick = true;
+        rg.isKinematic = true;
     }
     private void OnMouseUp()
     {
         isClick = false;
+        rg.isKinematic = false;
+        rg.constraints = RigidbodyConstraints.None;
+        Invoke("fly", 0.2f);
+    }
+
+    private void fly()
+    {
+        sp.spring = 0;
     }
 }
