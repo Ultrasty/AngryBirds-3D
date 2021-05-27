876968ad4e4d45cf237220a1beae6ce6a766827c
sty <1455670697@qq.com>
2021-05-27 11:00:31 +0800

    增加了死亡动画

---
+++ Assets/3D/Scripts/pig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pig : MonoBehaviour
+{
+    public float maxSpeed;
+    public GameObject boom;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > maxSpeed)
+        {
+            Destroy(transform.gameObject);
+            Instantiate(boom, transform.position, Quaternion.identity);
+        }
+    }
+}
