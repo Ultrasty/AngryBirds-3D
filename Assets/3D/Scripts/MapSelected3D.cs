a8d6fcee3aac85eaf586a25bd03d3558fb6e0bb0
sty <1455670697@qq.com>
2021-06-24 21:11:41 +0800

    小修小补

---
+++ Assets/3D/Scripts/MapSelected3D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelected3D : MonoBehaviour
+{
+
+    public GameObject map;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeSelected()
+    {
+        
+        map.SetActive(false);
+        panel.SetActive(true);
+    }
+}
